29306ae4079bd9b0e9a78c2356c2a8bdeb85db66
mhbahmani <mhbahmani79@gmail.com>
2022-05-03 22:06:29 +0430

    Replcae food name with food id

--- src/food_priorities_handler.py
+++ src/food_priorities_handler.py
@@ -1,9 +1,9 @@
-import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from src.messages import (
     next_page_button_message,
     previous_page_button_message,
-    done_button_message
+    done_button_message,
+    cancel_button_message
 )
 
 
@@ -11,19 +11,19 @@ PAGE_SIZE = 10
 
 class FoodPrioritiesHandler:
     @staticmethod
-    def create_food_list_keyboard(foods: set, page: int = 1) -> InlineKeyboardMarkup:
+    def create_food_list_keyboard(foods: list, page: int = 1) -> InlineKeyboardMarkup:
         keyboard = []
-        foods_list = list(foods)[(page - 1) * PAGE_SIZE:page * PAGE_SIZE]
+        foods_list = foods[(page - 1) * PAGE_SIZE:page * PAGE_SIZE]
 
         while foods_list:
             row = []
             for i in range(2):
                 if not foods_list: break
-                food = foods_list.pop()
+                food_id, food_name = foods_list.pop()
                 row.append(
                     InlineKeyboardButton(
-                        food,
-                        callback_data=FoodPrioritiesHandler.create_callback_data(action="SELECT", food_name=food)))
+                        food_name,
+                        callback_data=FoodPrioritiesHandler.create_callback_data(action="SELECT", food_name=food_id)))
             keyboard.append(row)
 
         row = []
@@ -36,6 +36,9 @@ class FoodPrioritiesHandler:
             row.append(
                 InlineKeyboardButton(
                     " ", callback_data=FoodPrioritiesHandler.create_callback_data("IGNORE")))
+        row.append(
+            InlineKeyboardButton(
+                cancel_button_message, callback_data=FoodPrioritiesHandler.create_callback_data("CANCEL")))
         row.append(
             InlineKeyboardButton(
                 done_button_message, callback_data=FoodPrioritiesHandler.create_callback_data("DONE")))

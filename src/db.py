5c0b78562080080f7facf682aba9c6adec41407f
mhbahmani <mhbahmani79@gmail.com>
2022-05-03 18:49:24 +0430

    Fix database name

--- src/db.py
+++ src/db.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 class DB:
 
     def __init__(self, host: str = "127.0.0.1", port: int = 27017) -> None:
-        self.db = MongoClient(host, int(port)).diningbot
+        self.db = MongoClient(host, int(port)).diningbotdb
 
     def add_user(self, user):
         self.db.users.insert_one(user)

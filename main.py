d69799b76464972d7afa7d2eff466c0b24497070
mhbahmani <mhbahmani79@gmail.com>
2022-05-03 17:43:39 +0430

    Add admin sharif sso username and password

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ DiningBot(
     db=DB(
         host=config('DB_HOST', default='127.0.0.1'),
         port=config('DB_PORT', default='27017')
-    )
+    ),
+    admin_sso_username=config('ADMIN_SHARIF_SSO_USERNAME'),
+    admin_sso_password=config('ADMIN_SHARIF_SSO_PASSWORD')
 ).run()

44452026455dec91093dca60f671ab7bb34b7c41
mhbahmani <mhbahmani79@gmail.com>
2022-05-14 19:13:53 +0430

    Fix code statistics return

--- src/forget_code.py
+++ src/forget_code.py
@@ -158,7 +158,7 @@ class ForgetCodeMenuHandler:
     def forget_code_statistics(self, update, context):
         update.message.reply_text(
             text=make_forget_code_statistics_message(list(self.db.get_forget_codes_by_food_court_id())))
-        return static_data.MAIN_MENU_CHOOSING
+        return static_data.FORGET_CODE_MENU_CHOOSING
 
     def handle_fake_forget_code_input(self, update, context):
         fake_forget_code = update.message.text

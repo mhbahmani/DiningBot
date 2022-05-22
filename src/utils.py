c790d4cbe3e60f61647c8a554e17a0c7bfbb56ff
mhbahmani <mhbahmani79@gmail.com>
2022-05-22 15:46:14 +0430

    Add updating .env file vars function

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import dotenv
 import src.static_data as static_data
 from src.messages import code_statistics_message, no_code_for_today_message, no_available_code_for_today_message
 
@@ -23,3 +24,7 @@ def make_forget_code_statistics_message(forget_codes: tuple) -> str:
     return code_statistics_message.format(
         used_forget_codes_list if used_forget_codes_list else no_code_for_today_message,
         unused_forget_codes_list if unused_forget_codes_list else no_available_code_for_today_message)
+
+def update_environment_variable(key: str, value):
+    dotenv_file = dotenv.find_dotenv()
+    dotenv.set_key(dotenv_file, key, value)

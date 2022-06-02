027d0e12f71685e86183cf77ac660598b676ec17
mhbahmani <mhbahmani79@gmail.com>
2022-06-02 16:42:11 +0430

    Update food courts inline handler

--- src/inline_keyboards_handlers/choose_food_courts_handler.py
+++ src/inline_keyboards_handlers/choose_food_courts_handler.py
@@ -7,7 +7,7 @@ from src.messages import (
 
 class FoodCourtSelectingHandler:
     @staticmethod
-    def create_food_courts_keyboard(food_courts: dict) -> InlineKeyboardMarkup:
+    def create_food_courts_keyboard(food_courts: dict, callback_key: str = "FOODCOURT") -> InlineKeyboardMarkup:
         keyboard = []
         food_courts = list(food_courts.items())
         food_courts.reverse()
@@ -21,23 +21,23 @@ class FoodCourtSelectingHandler:
                     InlineKeyboardButton(
                         food_court_name,
                         callback_data=FoodCourtSelectingHandler.create_callback_data(
-                            action="SELECT", food_court_id=food_court_id)))
+                            action="SELECT", food_court_id=food_court_id, callback_key=callback_key)))
             keyboard.append(row)
 
         row = []
         row.append(
             InlineKeyboardButton(
-                cancel_button_message, callback_data=FoodCourtSelectingHandler.create_callback_data("CANCEL")))
+                cancel_button_message, callback_data=FoodCourtSelectingHandler.create_callback_data("CANCEL", callback_key=callback_key)))
         row.append(
             InlineKeyboardButton(
-                done_button_message, callback_data=FoodCourtSelectingHandler.create_callback_data("DONE")))
+                done_button_message, callback_data=FoodCourtSelectingHandler.create_callback_data("DONE", callback_key=callback_key)))
         keyboard.append(row)
 
         return InlineKeyboardMarkup(keyboard)
 
     @staticmethod
-    def create_callback_data(action: str, food_court_id: str = "-") -> str:
-        return "FOODCOURT" + ";" + ";".join([action, food_court_id])
+    def create_callback_data(action: str, food_court_id: str = "-", callback_key: str = "FOODCOURT") -> str:
+        return callback_key + ";" + ";".join([action, food_court_id])
 
     @staticmethod
     def separate_callback_data(data: str) -> list:

0c78c496feeac3ef6db212718c4b01cb0558d5a9
mhbahmani <mhbahmani79@gmail.com>
2023-10-17 00:18:26 +0330

    Change automatic reserve scheduling

--- automatic_reserve_main.py
+++ automatic_reserve_main.py
@@ -22,10 +22,10 @@ if __name__ == '__main__':
             ),
         )
 
-    schedule.every().monday.at("10:00").do(automatic_reserve_handler.clean_reservation_status)
+    schedule.every().tuesday.at("10:00").do(automatic_reserve_handler.clean_reservation_status)
 
-    schedule.every().monday.at("14:00").do(automatic_reserve_handler.notify_users)
-    schedule.every().monday.at("16:00").do(automatic_reserve_handler.handle_automatic_reserve)
+    # schedule.every().monday.at("14:00").do(automatic_reserve_handler.notify_users)
+    # schedule.every().monday.at("16:00").do(automatic_reserve_handler.handle_automatic_reserve)
     schedule.every().tuesday.at("14:00").do(automatic_reserve_handler.notify_users)
     schedule.every().tuesday.at("16:00").do(automatic_reserve_handler.handle_automatic_reserve)
     schedule.every().wednesday.at("14:00").do(automatic_reserve_handler.notify_users)

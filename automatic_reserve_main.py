79b412bf9c85190735a895fea9e246774eeb8f00
mhbahmani <mhbahmani79@gmail.com>
2023-10-17 22:09:28 +0330

    Change sleep time

--- automatic_reserve_main.py
+++ automatic_reserve_main.py
@@ -35,4 +35,4 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     while True:
         loop.run_until_complete(schedule.run_pending())
-        time.sleep(1)
+        time.sleep(60 * 10)
